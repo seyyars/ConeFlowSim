a102d97997a904a989bf0ac9f132878d71aa1767
seyed arsham asgari <s.a.asg2000@gmail.com>
2025-12-05 02:45:07 +0330

    Update __init__.py

--- src/coneflowsim/__init__.py
+++ src/coneflowsim/__init__.py
@@ -3,12 +3,10 @@ ConeFlowSim: educational tools for supersonic flow over sharp cones
 (wedge approximation + Taylor–Maccoll cone model).
 """
 
-from .wedge import (
-    oblique_shock,
-    wedge_field,
-    wedge_plot,
-)
+# مدل گوه (wedge)
+from .wedge import oblique_shock, wedge_field, wedge_plot
 
+# مدل Taylor–Maccoll
 from .taylormaccoll import (
     taylor_maccoll_solution,
     cone_field_tm,

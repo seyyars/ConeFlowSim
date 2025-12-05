087c8ee566db74310cd1e7a3cb840f7312713b63
seyed arsham asgari <s.a.asg2000@gmail.com>
2025-12-05 12:16:42 +0330

    Update __init__.py

--- src/coneflowsim/__init__.py
+++ src/coneflowsim/__init__.py
@@ -1,7 +1,16 @@
-from .wedge import oblique_shock, wedge_field, wedge_plot
+from .wedge import (
+    oblique_shock,
+    wedge_field,
+    wedge_plot,
+)
+
 from .taylormaccoll import (
+    taylor_maccoll_rhs,       # اگر دوست داری این را هم بیرون نشان بدهی
     taylor_maccoll_solution,
     cone_field_tm,
+)
+
+from .comparison_plot import (
     comparison_plot,
 )
 
@@ -9,6 +18,7 @@ __all__ = [
     "oblique_shock",
     "wedge_field",
     "wedge_plot",
+    "taylor_maccoll_rhs",      # اختیاری
     "taylor_maccoll_solution",
     "cone_field_tm",
     "comparison_plot",
